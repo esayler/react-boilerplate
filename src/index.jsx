9a0dbbb0fc4c6a90dff09117afdc92b9375add6b
Eric Sayler <easayler@gmail.com>
2017-01-29 12:39:21 -0700

    add back styles importstatement

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import { AppContainer as HotReloader } from 'react-hot-loader'
 import Root from './components/Root'
 
+require('./styles/base.scss')
+
 const rootElement = document.getElementById('root')
 ReactDOM.render(
   <HotReloader>
